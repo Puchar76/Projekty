9266203d7d4880b8eccbeb7698777c3299cd2543
Puchar76 <TomekP2009>
2024-03-13 23:53:01 +0100

    Praca domowa moduł 4

---
+++ Program/Program/Program.cs
@@ -0,0 +1,40 @@
+﻿var name = "Ewa";
+string plec = "man";
+int wiek = 17;
+
+if (wiek < 30)
+{
+    if (plec == "kobieta")
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat.");
+    }
+    else if (plec == "man" && wiek < 18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna.");
+    }
+
+}
+else if (wiek == 30 && name == "Ewa")
+{
+    Console.WriteLine("Kobieta, lat 30.");
+}
+
+
+
+
+//if (plec == "kobieta" && wiek < 30)
+//{
+//    Console.WriteLine("Kobieta poniżej 30 lat.");
+//}
+//else if (name == "Ewa" && wiek == 30)
+//{
+//    Console.WriteLine("Ewa, lat 30.");
+//}
+//else if (plec == "man" && wiek < 18)
+//{
+//    Console.WriteLine("Niepełnoletni mężczyzna.");
+//}
+//else
+//{
+//    Console.WriteLine("Brak wyników.");
+//}
